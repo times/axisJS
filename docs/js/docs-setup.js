e2c16858367e72744e8005d24e5277017b65d4a5
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2014-08-13 01:04:28 +0100

    Adding docs.

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,51 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "axisJSApp",
+      "shortName": "axisJSApp",
+      "type": "overview",
+      "moduleName": "axisJSApp",
+      "shortDescription": "axisJSApp",
+      "keywords": "api application axisjsapp main module overview"
+    },
+    {
+      "section": "api",
+      "id": "axisJSApp.controller:MainCtrl",
+      "shortName": "MainCtrl",
+      "type": "function",
+      "moduleName": "axisJSApp",
+      "shortDescription": "Main controller. Populates and links the input fields.",
+      "keywords": "api axisjsapp controller fields function input links main populates"
+    },
+    {
+      "section": "api",
+      "id": "axisJSApp.directive:BuildChart",
+      "shortName": "BuildChart",
+      "type": "directive",
+      "moduleName": "axisJSApp",
+      "shortDescription": "Base directive for building the chart preview.",
+      "keywords": "api axisjsapp base building chart directive preview"
+    },
+    {
+      "section": "api",
+      "id": "axisJSApp.directive:exportChart",
+      "shortName": "exportChart",
+      "type": "directive",
+      "moduleName": "axisJSApp",
+      "shortDescription": "Most of this is shamelessly stolen from Quartz&#39;s ChartBuilder.",
+      "keywords": "api axisjsapp chartbuilder directive hell quartz refactor shamelessly stolen"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
---
+++ docs/pages.js
@@ -0,0 +1,8 @@
+NG_DOCS={
+  "sections": {},
+  "pages": [],
+  "apis": {},
+  "title": "",
+  "html5Mode": true,
+  "startPage": "/api"
+};
