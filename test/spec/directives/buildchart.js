3cd2cfff312e023cd15c5191ac040225e50d71bb
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2014-09-26 17:09:03 +0100

    Adding more unit tests.

--- test/spec/directives/buildchart.js
+++ test/spec/directives/buildchart.js
@@ -25,4 +25,17 @@ describe('Directive: BuildChart', function () {
       expect(element.children('svg')).toBeTruthy();
     }, 500);
   }));
+
+  // @TODO This probably won't work until after I abstract redraw() into a Service.
+  // @sa #15.
+
+  // it('should fire redraw when sundry config options change', inject(function($compile){
+  //   var element = angular.element('<div id="chart" build-chart></div>');
+  //   element = $compile(element)(scope);
+  //   scope.$apply();
+  //
+  //   setTimeout(function(){
+  //     expect(element.children('svg')).toBeTruthy();
+  //   }, 500);
+  // }));
 });
