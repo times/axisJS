27c79b611b2a0e7dd08a388dcbf446844f5eed5a
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2015-01-26 18:12:45 +0000

    Bumping version.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,7 +28,7 @@ angular
         controller: 'MainCtrl',
         resolve: {
           appConfig: ['configProvider', '$rootScope', function(configProvider, $rootScope) {
-            $rootScope.version = '1.0.0';
+            $rootScope.version = '1.0.1';
             return configProvider;
           }]
         }
