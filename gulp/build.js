431afadd575de6f24cb1c34fe993dd59ebdfa486
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2015-07-08 15:01:41 +0100

    Fixing gulp tasks for dist.

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'axisJs',
+      module: 'axis',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
