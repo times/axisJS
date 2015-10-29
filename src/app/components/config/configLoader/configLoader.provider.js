b892cfa312b438edc8fc74fb6979d4c2e5978772
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2015-10-29 14:48:27 +0000

    Adds the ability to override default config.

--- src/app/components/config/configLoader/configLoader.provider.js
+++ src/app/components/config/configLoader/configLoader.provider.js
@@ -21,15 +21,28 @@
       $get: function($http, $q, localStorageService, $window) {
         var jsyaml = $window.jsyaml;
 
-        var defaultConfig = $http.get('default.config.yaml');
-        var userConfigFile = localStorageService.get('config') ? localStorageService.get('config') : 'config.yaml';
-        var userConfig = $http.get(userConfigFile).then(
+        var defaultConfigFile = localStorageService.get('defaultConfig') ? localStorageService.get('defaultConfig') : 'default.config.yaml';
+        var defaultConfig = $http.get(defaultConfigFile).then(
           function(response){
-            return response.data;
+            return response;
           },
           function(response){
             if (response.status === 404) {
-              response.data = {};
+              return $http.get('default.config.yaml');
+            } else {
+              return $q.reject(response);
+            }
+          }
+        );
+
+        var userConfigFile = localStorageService.get('config') ? localStorageService.get('config') : 'config.yaml';
+        var userConfig = $http.get(userConfigFile).then(
+          function(response){
+            return response;
+          },
+          function(response){
+            if (response.status === 404) {
+              response = {};
               return response;
             } else {
               return $q.reject(response);
@@ -39,12 +52,15 @@
 
         return $q.all([defaultConfig, userConfig]).then(function(values){
           var defaultConfigYaml = jsyaml.safeLoad(values[0].data);
-          var userConfigYaml = jsyaml.safeLoad(values[1]);
+          console.dir(defaultConfigYaml);
+          var userConfigYaml = jsyaml.safeLoad(values[1].data);
+          console.dir(userConfigYaml);
           var axisConfig;
 
           // Oddly, js-yaml returns string 'undefined' on fail and not type undefined
           userConfigYaml = userConfigYaml !== 'undefined' ? userConfigYaml : {};
-          axisConfig = angular.extend(defaultConfigYaml, userConfigYaml);
+          axisConfig = angular.extend({}, defaultConfigYaml, userConfigYaml);
+          console.dir(axisConfig);
 
           axisConfig.framework = axisConfig.renderer; // Needed for backwards compat.
           return axisConfig;
